996670dab4de035e0864389e32b2128ac787aa3a
luke01455 <luke0145555@hotmail.co.uk>
2019-12-03 20:46:59 +0000

    added avatars for the friends list, some minor styling, and implemented the touchable opacity import

--- ChatMobileApp/screens/FriendListScreen.js
+++ ChatMobileApp/screens/FriendListScreen.js
@@ -1,24 +1,43 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
 import { useSelector } from 'react-redux';
-import { FlatList } from 'react-native-gesture-handler';
 
 const FriendListScreen = () => {
     const usersOnline = useSelector(state => state.usersOnline);
     console.log("usersOnline", usersOnline);
 
+    const { itemContainerStyle, avatarImgStyle, userNameViewStyle } = styles;
+
     return (
-        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <View style={{ flex: 1 }}>
             <FlatList 
                 data={usersOnline}
                 renderItem={({item}) =>{
                     console.log("item", item);
-                return <Text>{item.userName}</Text>
+                return (
+                    <TouchableOpacity>
+                    <View style={itemContainerStyle}>
+                        <Image 
+                        style={avatarImgStyle}
+                        source={{ uri: item.avatar }}
+                        />
+                        <View style={userNameViewStyle}>
+                        <Text style={{ fontSize: 20 }}>{item.userName}</Text>
+                        </View>
+                    </View> 
+                    </TouchableOpacity>
+                )
                 }}
-                keyExtractor={item => item.userName}
+                keyExtractor={item => item.userId}
             />
         </View>
     );
 }
+
+const styles = StyleSheet.create ({
+    itemContainerStyle: { flex: 1, flexDirection: 'row' },
+    avatarImgStyle: { height: 100, width: 100, borderRadius: 50 },
+    userNameViewStyle: { flex: 1, justifyContent: 'center', alignItems: 'center' }
+});
 
 export default FriendListScreen;
